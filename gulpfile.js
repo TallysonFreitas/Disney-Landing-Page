a7f6eabe61d0911079076f20e4247ab2d1174a86
Tallyson Freitas <tallyson.eu@outlook.com>
2023-06-29 14:36:42 -0300

    uglify js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp')
 
 const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
+
+function JavaScript(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts/'))
+}
 
 function Sass(){
     return gulp.src('./src/styles/*.scss')
@@ -17,7 +24,8 @@ function Img(){
         .pipe(gulp.dest('./dist/img/'))
 }
 
-exports.default = gulp.parallel(Sass,Img)
+exports.default = gulp.parallel(Sass,Img,JavaScript)
 exports.watch = function () {  
     gulp.watch('./src/styles/*.scss', gulp.parallel(Sass))
+    gulp.watch('./src/scripts/main.js', gulp.parallel(JavaScript))
 }
